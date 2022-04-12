222056b8519e9a6a3b2389753d203f2d9829a180
JESSE <dtrsjesse121@gmail.com>
2022-04-12 19:50:07 -0400

    add view employee department function to viewTables.js

--- utils/viewTables.js
+++ utils/viewTables.js
@@ -47,4 +47,20 @@ const viewEmployees = () => {
     });
 }
 
-module.exports = {viewDepartments, viewEmployees, viewRoles}
+const viewEmployeeDepartment = () => {
+    console.log('Showing employee by departments...\n');
+    const sql = `SELECT employee.first_name, 
+                        employee.last_name, 
+                        department.name AS department
+                 FROM employee 
+                 LEFT JOIN role ON employee.role_id = role.id 
+                 LEFT JOIN department ON role.department_id = department.id`;
+  
+    connection.query(sql, (err, rows) => {
+      if (err) throw err; 
+      console.table(rows); 
+      index.init();
+    });          
+  };
+
+module.exports = {viewDepartments, viewEmployees, viewRoles, viewEmployeeDepartment}
